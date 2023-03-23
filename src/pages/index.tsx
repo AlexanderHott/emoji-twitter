ab200ed08d270eb8004a7e869eb80afcb7dbcc90
Alex Ott <66271487+AlexanderHOtt@users.noreply.github.com>
2023-03-23 10:38:12 -0700

    add db schema and post router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,8 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+  const { data: posts } = api.post.getAll.useQuery();
 
   return (
     <>
@@ -17,8 +17,15 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {!!user.isSignedIn && <SignOutButton />}
-        {!user.isSignedIn && <SignInButton />}
+        <div>
+          {!!user.isSignedIn && <SignOutButton />}
+          {!user.isSignedIn && <SignInButton />}
+        </div>
+        <div>
+          {posts?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,13 +1,12 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
-
+import { postsRouter } from "~/server/api/routers/posts";
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  post: postsRouter,
 });
 
 // export type definition of API
