4348b687335f60160ad195680e408503b21167c5
AlexanderHOtt <aott01@protonmail.ch>
2023-06-16 12:36:05 -0700

    edit debug page

--- src/pages/debug.tsx
+++ src/pages/debug.tsx
@@ -1,151 +1,57 @@
 const DebugPage = () => {
+  const vars = [
+    "VERCEL",
+    "CI",
+    "VERCEL_ENV",
+    "VERCEL_URL",
+    "VERCEL_BRANCH_URL",
+    "VERCEL_REGION",
+    "VERCEL_GIT_PROVIDER",
+    "VERCEL_GIT_REPO_SLUG",
+    "VERCEL_GIT_REPO_OWNER",
+    "VERCEL_GIT_REPO_ID",
+    "VERCEL_GIT_COMMIT_REF",
+    "VERCEL_GIT_COMMIT_SHA",
+    "VERCEL_GIT_COMMIT_MESSAGE",
+    "VERCEL_GIT_COMMIT_AUTHOR_LOGIN",
+    "VERCEL_GIT_COMMIT_AUTHOR_NAME",
+    "VERCEL_GIT_PREVIOUS_SHA",
+    "VERCEL_GIT_PULL_REQUEST_ID",
+    "NEXT_PUBLIC_VERCEL_ENV",
+    "NEXT_PUBLIC_VERCEL_URL",
+    "NEXT_PUBLIC_VERCEL_BRANCH_URL",
+    "NEXT_PUBLIC_VERCEL_GIT_PROVIDER",
+    "NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG",
+    "NEXT_PUBLIC_VERCEL_GIT_REPO_OWNER",
+    "NEXT_PUBLIC_VERCEL_GIT_REPO_ID",
+    "NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF",
+    "NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA",
+    "NEXT_PUBLIC_VERCEL_GIT_COMMIT_MESSAGE",
+    "NEXT_PUBLIC_VERCEL_GIT_COMMIT_AUTHOR_LOGIN",
+    "NEXT_PUBLIC_VERCEL_GIT_COMMIT_AUTHOR_NAME",
+    "NEXT_PUBLIC_VERCEL_GIT_PULL_REQUEST_ID",
+  ];
   return (
-
     <div className="flex justify-center">
       <table className="border border-slate-600">
         <tr>
-          <th>Env var</th>
-          <th>Value</th>
+          <th>Var</th>
+          <th>Val</th>
         </tr>
-        <tr>
-          <td>
-            <pre>process.env.NODE_ENV</pre>
-          </td>
-          <td>
-            <pre>{process.env.NODE_ENV}</pre>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <pre>process.env.VERCEL</pre>
-          </td>
-          <td>
-            <pre>{process.env.VERCEL}</pre>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <pre>VERCEL_ENV</pre>
-          </td>
-          <td>
-            <pre>{process.env.VERCEL_ENV}</pre>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <pre>VERCEL_URL</pre>
-          </td>
-          <td>
-            <pre>{process.env.VERCEL_URL}</pre>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <pre>VERCEL_BRANCH_URL</pre>
-          </td>
-          <td>
-            <pre>{process.env.VERCEL_BRANCH_URL}</pre>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <pre>VERCEL_REGION</pre>
-          </td>
-          <td>
-            <pre>{process.env.VERCEL_REGION}</pre>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <pre>VERCEL_GIT_PROVIDER</pre>
-          </td>
-          <td>
-            <pre>{process.env.VERCEL_GIT_PROVIDER}</pre>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <pre>VERCEL_GIT_REPO_SLUG</pre>
-          </td>
-          <td>
-            <pre>{process.env.VERCEL_GIT_REPO_SLUG}</pre>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <pre>VERCEL_GIT_REPO_OWNER</pre>
-          </td>
-          <td>
-            <pre>{process.env.VERCEL_GIT_REPO_OWNER}</pre>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <pre>VERCEL_GIT_REPO_ID</pre>
-          </td>
-          <td>
-            <pre>{process.env.VERCEL_GIT_REPO_ID}</pre>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <pre>VERCEL_GIT_COMMIT_REF</pre>
-          </td>
-          <td>
-            <pre>{process.env.VERCEL_GIT_COMMIT_REF}</pre>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <pre>VERCEL_GIT_COMMIT_SHA</pre>
-          </td>
-          <td>
-            <pre>{process.env.VERCEL_GIT_COMMIT_SHA}</pre>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <pre>VERCEL_GIT_COMMIT_MESSAGE</pre>
-          </td>
-          <td>
-            <pre>{process.env.VERCEL_GIT_COMMIT_MESSAGE}</pre>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <pre>VERCEL_GIT_COMMIT_AUTHOR_LOGIN</pre>
-          </td>
-          <td>
-            <pre>{process.env.VERCEL_GIT_COMMIT_AUTHOR_LOGIN}</pre>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <pre>VERCEL_GIT_COMMIT_AUTHOR_NAME</pre>
-          </td>
-          <td>
-            <pre>{process.env.VERCEL_GIT_COMMIT_AUTHOR_NAME}</pre>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <pre>VERCEL_GIT_PREVIOUS_SHA</pre>
-          </td>
-          <td>
-            <pre>{process.env.VERCEL_GIT_PREVIOUS_SHA}</pre>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <pre>VERCEL_GIT_PULL_REQUEST_ID</pre>
-          </td>
-          <td>
-            <pre>{process.env.VERCEL_GIT_PULL_REQUEST_ID}</pre>
-          </td>
-        </tr>
+        {vars.map((v) => {
+          return (
+            <tr>
+              <td className="border border-slate-600 p-1">
+                <pre>{v}</pre>
+              </td>
+              <td className="border border-slate-600 p-1">
+                <pre>{process.env[v]}</pre>
+              </td>
+            </tr>
+          );
+        })}
       </table>
     </div>
   );
 };
-
 export default DebugPage;
