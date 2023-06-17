e68a7dee4e22ee4f95b4234e35587121c5fa5f88
AlexanderHOtt <aott01@protonmail.ch>
2023-06-17 08:44:23 -0700

    update debug page

--- src/pages/debug.tsx
+++ src/pages/debug.tsx
@@ -1,8 +1,19 @@
-const DebugPage = () => {
-  return (
-    <div className="flex justify-center">
-      {process.env.VERCEL_URL}
-    </div>
-  );
+import type { GetServerSideProps, InferGetServerSidePropsType } from "next";
+
+export const getServerSideProps: GetServerSideProps<{
+  vercel_url?: string;
+}> = async () => {
+  return {
+    props: {
+      vercel_url: process.env.VERCEL_URL,
+    },
+  };
+};
+
+const DebugPage = ({
+  vercel_url,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  console.log(process.env.NEXT_PUBLIC_VERCEL_URL)
+  return <div className="flex justify-center">{vercel_url}</div>;
 };
 export default DebugPage;
