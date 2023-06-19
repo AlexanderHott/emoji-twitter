4bc733b17f7004e8bf8097fa19fff869c3fa0c97
AlexanderHOtt <aott01@protonmail.ch>
2023-06-19 02:41:15 -0700

    update files

--- src/server/utils.ts
+++ src/server/utils.ts
@@ -1,5 +1,5 @@
 import { type User } from "@clerk/nextjs/api";
-import { createProxySSGHelpers } from "@trpc/react-query/ssg";
+import { createServerSideHelpers } from "@trpc/react-query/server";
 import superjson from "superjson";
 import { appRouter } from "~/server/api/root";
 import { prisma } from "~/server/db";
@@ -13,7 +13,7 @@ export const filterUserForClient = (user: User) => {
 };
 
 export const generateSSGHelper = () => {
-  return createProxySSGHelpers({
+  return createServerSideHelpers({
     router: appRouter,
     ctx: { prisma: prisma, userId: null },
     transformer: superjson, // optional - adds superjson serialization
